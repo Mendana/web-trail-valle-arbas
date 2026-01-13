13a07f5865cd28d3f30808835c30ae7759bbb330
mendana <diegodiazmenda@gmail.com>
2026-01-13 17:35:00 +0100

    SEO improvement

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import tailwind from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://web-trail-valle-arbas.web.app/",
   vite: {
     plugins: [tailwind()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
